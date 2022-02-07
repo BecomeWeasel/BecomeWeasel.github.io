0fb32f8e9fa83ffa15ed309c31ad6ac11faaf2a7
BecomeWeasel <hsw0194@gmail.com>
2022-02-07 19:44:09 +0900

    404 페이지 수정

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import Layout from '../layout';
 import Seo from '../components/seo';
+import Logo from '../../static/404.jpg';
 
 function NotFoundPage() {
   return (
     <Layout>
       <Seo title="404: Not found" />
       <h1>404: Not Found</h1>
-      <img src="./../../static/404.jpg"/>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      
+      <img width={540} height={675} src={Logo} alt={'404logo'} />
     </Layout>
   );
 }
